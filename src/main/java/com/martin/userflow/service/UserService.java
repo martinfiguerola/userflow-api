4a22b090699f17e123fa456b265f1ce62b9a0e7d
martinfiguerola <fedefiguerola@gmail.com>
2025-05-19 08:15:22 +0200

    feat: add findAll method to UserService and implementation

--- src/main/java/com/martin/userflow/service/UserService.java
+++ src/main/java/com/martin/userflow/service/UserService.java
@@ -3,9 +3,12 @@ package com.martin.userflow.service;
 import com.martin.userflow.dto.UserRequestDTO;
 import com.martin.userflow.dto.UserResponseDTO;
 
+import java.util.List;
+
 
 public interface UserService {
 
     UserResponseDTO save (UserRequestDTO userRequestDTO);
+    List<UserResponseDTO> findAll ();
 
 }
