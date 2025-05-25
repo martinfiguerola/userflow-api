dd8a98fc001d9b740cdfb05ab2a375da89b0de1c
martinfiguerola <fedefiguerola@gmail.com>
2025-05-25 10:12:23 +0200

    feat: add update method to UserService and its implementation

--- src/main/java/com/martin/userflow/service/UserService.java
+++ src/main/java/com/martin/userflow/service/UserService.java
@@ -4,11 +4,13 @@ import com.martin.userflow.dto.UserRequestDTO;
 import com.martin.userflow.dto.UserResponseDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserService {
 
     UserResponseDTO save (UserRequestDTO userRequestDTO);
     List<UserResponseDTO> findAll ();
+    Optional<UserResponseDTO> update (Long id, UserRequestDTO userRequestDTO);
 
 }
